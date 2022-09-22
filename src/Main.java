c93c8a4b7d32e8bc7b6165bf170b22081a21a2c8
Hectorgym6 <elcorreodetuentrenadorpersonal@gmail.com>
2022-09-22 11:59:48 +0200

    ac

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Accumulator accumulator = new Accumulator(0);
+        //Crear lista de HIlos (linkedList)
+        //Para acceder a un .get utilizaremos un arraylist porque es mas rapido. 
 
         for (int i = 0; i < 100; i++) {
 
             Thread incrementThread = new Thread(new IncrementTask(accumulator, 1000));
             incrementThread.start();
+            //Añadir el HIlo a la Lista
         }
         try {
+            //HAcer join de todos los hilos de la lista
             Thread.sleep(15000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
