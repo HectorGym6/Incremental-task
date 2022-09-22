1ef8ecb48070dbea42e3756b70cd555a671ac170
Hectorgym6 <elcorreodetuentrenadorpersonal@gmail.com>
2022-09-22 11:50:14 +0200

    Actualizacion 22 sep

--- src/Accumulator.java
+++ src/Accumulator.java
@@ -1,2 +1,8 @@
 public class Accumulator {
+    private int value;
+     public Accumulator(int value){
+         this.value = value;
+     }
+     public void increment(){ value = value + 1;}
+    public int getValue() { return value; }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Accumulator accumulator = new Accumulator(0);
+
+        for (int i = 0; i < 100; i++) {
+
+            Thread incrementThread = new Thread(new IncrementTask(accumulator, 1000));
+            incrementThread.start();
+        }
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(accumulator.getValue());
     }
 }
